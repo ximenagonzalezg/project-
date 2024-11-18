83c1a8c1fd68f051523bf43b712b3fea66d406d8
ximenagonzalezg <A01708366@tec.mx>
2024-11-17 22:23:24 -0600

    Update Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -1,11 +1,27 @@
 #include "Person.h"
-Person::Person(int_id, string_name, string_phone){
-    id=_id 
-    name=_name 
-    phone=_phone
+Person::Person(int _id, string _name, string _phone){
+    id = _id; 
+    name = _name; 
+    phone = _phone; 
 }
-string Person::showInfo(){
-    return id
-    return name
-    return phone
+
+void Person::setPhone(string _phone){
+    phone = _phone; 
+}
+
+int Person::getId(){
+    return id; 
+}
+
+string Person::getName(){ 
+    return name;
+}
+
+string Person::getPhone(){
+    return phone;
+}
+    
+string Person::getDetails(){
+    string details = "ID: " + to_string (id) + "\nName: " + name + "\nPhone: " + phone + "\n------\n";
+    return details; 
 }
