f4ecb06bb406927b54a1a2df4f483373dc1ac205
ximenagonzalezg <A01708366@tec.mx>
2024-11-17 22:28:34 -0600

    Update Patient.h

--- Patient.h
+++ Patient.h
@@ -1,11 +1,25 @@
 #include <string> 
 using namespace std;
 
-class Patient{
+#ifndef PATIENT_H
+#define PATIENT_H
+
+#include "Person.h"
+#include "Medical history.cpp"
+
+class Patient: public Person{
     private:
         int age; 
-        string address;  
+        string address;
+        MedicalHistory medicalHistory; 
+          
     public: 
+        Patient (int _id, string _name, string _phone, int _age, string _address); 
         void setAge(int);
         void setAddress(string);
+        int getAge (); 
+        string getAddress (); 
+        MedicalHistory getMedicalHistory (); 
+        string getDetails(); 
 }; 
+#endif 
