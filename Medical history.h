02a55e7313e8c368a8b990e58f03660243d052e9
ximenagonzalezg <A01708366@tec.mx>
2024-11-17 22:44:41 -0600

    Update Medical history.h

--- Medical history.h
+++ Medical history.h
@@ -1,13 +1,17 @@
 #include <string> 
 using namespace std;
 
+#ifndef MEDICAL HISTORY_H
+#define MEDICAL HISTORY_H
+
 class MedicalHistory{
     private: 
-        int idPacient; 
         string diagnosis; 
         string treatment; 
     public: 
+        MedicalHistory (); 
         void setDiagnosis (string); 
         void setTreatment (string); 
-        string showHistory (); 
+        string getDetails (); 
 }; 
+#endif
