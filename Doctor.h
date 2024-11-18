2449a4925d008f4bf755d2dfe51a6f5f1f4f9fa6
ximenagonzalezg <A01708366@tec.mx>
2024-11-17 23:45:00 -0600

    Update Doctor.h

--- Doctor.h
+++ Doctor.h
@@ -1,9 +1,16 @@
 #include <string> 
+#include <vector>
 using namespace std;
+#include "Person.h"
 
-class Doctor{
+class Doctor: public Person{
     private:
-        string specialty;   
+        string specialty;  
+        Department department;  
     public: 
-        string obtainSpecialty();
+        Doctor (int _id, string _name, string _phone, string _specialty, Department department); 
+        string getSpecialty();
+        void setDepartment (department);
+        string getDepartment(); 
+        string getDetails(); 
 }; 
