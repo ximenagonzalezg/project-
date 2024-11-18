037f441e75582f2997ddfd7ae1b1f5ba98c27806
ximenagonzalezg <A01708366@tec.mx>
2024-11-17 23:07:10 -0600

    Update Patient.cpp

--- Patient.cpp
+++ Patient.cpp
@@ -1,7 +1,29 @@
 #include "Patient.h"
-void Patient::setAge(string_age){
-    age=_age
+Patient::Patient(int _id, string _name, string _phone, int _age, string _address):Person(_id,_name,_phone){
+    age = _age; 
+    address = _address; 
 }
-void Patient::setAddress(string_address){
-    address=_address
+
+void Patient::setAge(int _age){
+    age = _age;
 }
+
+void Patient::setAddress (string _address){
+    address = _address; 
+} 
+
+int Patient::getAge(){ 
+    return age;
+}
+
+string Patient::getAddress(){ 
+    return address;
+}
+
+MedicalHistory Patient::getMedicalHistory(){
+    return medicalHistory; 
+}
+
+string Patient::getDetails(){
+   string details = "Age: " + to_string (age) + "\nAddress: " + address + "\nMedical History:" + medicalHistory.getDetails() + "\n------\n";
+   return details;  
