91175452bf8daa4d8db5b09221553139a6fc2085
ximenagonzalezg <A01708366@tec.mx>
2024-11-22 23:31:21 -0600

    Update Doctor.cpp

--- Doctor.cpp
+++ Doctor.cpp
@@ -1,22 +1,8 @@
 #include "Doctor.h"
-Doctor::Doctor (int _id, string _name, string _phone, string _specialty, Department _department):Person(_id,_name,_phone){
-    specialty = _specialty; 
-    department = _department;
+Doctor::Doctor (int _id, string _name, string _phone, string _specialty):Person(_id,_name,_phone){
+    specialty = _specialty;
 }
-
+   
 string Doctor::getSpecialty(){
     return specialty; 
-}
-
-void Doctor::setDepartment(_department){
-    department = _department
-}
-
-string Doctor::getDepartment(){
-    return department; 
-}
-
-string Doctor::getDetails(){
-    string details = "Specialty: " + specialty+ "\nDepartment: " + department.getName() + "\n------\n";
-    return details;
 }
