5f4e3b1d7d0664946640fcf6928c08a4609965a9
ximenagonzalezg <A01708366@tec.mx>
2024-11-22 23:54:49 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,14 +7,13 @@
 #include "Department.cpp"
 #include "Patient.cpp"
 
-int main() 
-{
-    Patient p1 (17896, "Ximena", "442 8307 639", 18, "Milenio III"); 
+int main() {
+    Patient p1 (17896, "Ximena González", "442 8307 639", 18, "Milenio III"); 
 
     Department dpt("Cardiology");
 
-    Doctor d1 (78456, "Genaro", "442 114 7854", "Cardiology"); 
-    Doctor d2 (2446, "Leticia", "442 6789 234", "Nutrition"); 
+    Doctor d1 (78456, "Dr. Genaro", "442 114 7854", "Cardiology"); 
+    Doctor d2 (2446, "Dr. Leticia", "442 6789 234", "Nutrition"); 
 
     dpt.addDoctor(d1);
     dpt.addDoctor(d2);
@@ -24,6 +23,18 @@ int main()
     for (size_t i = 0; i < doctors.size(); i++) {
         cout << " - " << doctors[i].getName() << " (" << doctors[i].getSpecialty() << ")\n";
     }
+
+    MedicalHistory medicalHistory1;
+    medicalHistory1.setDiagnosis("Heart Condition");
+    medicalHistory1.setTreatment("Regular check-ups and medication");
+    p1.setMedicalHistory(medicalHistory1);
+
+    cout << "\nPatient Details:\n";
+    cout << "Name: " << p1.getName() << "\n";
+    cout << "Age: " << p1.getAge() << "\n";
+    cout << "Address: " << p1.getAddress() << "\n";
+    cout << "Diagnosis: " << p1.getMedicalHistory().getDiagnosis() << "\n";
+    cout << "Treatment: " << p1.getMedicalHistory().getTreatment() << "\n";
 
     Appointment apt (p1, d1);
     apt.setDate("2024-11-23");
