03e2356462cc23bd8c9d857cf76d173318192108
ximenagonzalezg <A01708366@tec.mx>
2024-11-22 23:46:21 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@
 
 int main() 
 {
+    Patient p1 (17896, "Ximena", "442 8307 639", 18, "Milenio III"); 
+
     Department dpt("Cardiology");
 
     Doctor d1 (78456, "Genaro", "442 114 7854", "Cardiology"); 
@@ -23,24 +25,18 @@ int main()
         cout << " - " << doctors[i].getName() << " (" << doctors[i].getSpecialty() << ")\n";
     }
 
-    Patient p1 (17896, "Ximena", "442 8307 639", 18, "Milenio III"); 
-
-    // Create an appointment
     Appointment apt (p1, d1);
     apt.setDate("2024-11-23");
     apt.setTime("10:00 AM");
 
-    // Print appointment details
     cout << "\nAppointment Details:\n";
     cout << "Patient: " << p1.getName() << "\n";
     cout << "Doctor: " << apt.getDoctor().getName() << "\n";
     cout << "Date: " << apt.getDate() << "\n";
     cout << "Time: " << apt.getTime() << "\n";
 
-    // Remove a doctor from the department
     dpt.deleteDoctor(d2);
 
-    // Print updated list of doctors in the department
     cout << "\nUpdated Doctors in " << dpt.getName() << " department:\n";
     doctors = dpt.showDoctors();
     for (size_t i = 0; i < doctors.size(); i++) {
